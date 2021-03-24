958eecebb1eb7ac711d3c3c70eb4c03f9b9ee5cb
ducanhub00 <ducanhkhtn@gmail.com>
2021-03-24 22:08:21 +0700

    code linearSearch

--- Online-LinearSearch/src/App.java
+++ Online-LinearSearch/src/App.java
@@ -1,7 +1,53 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        System.out.println("hello");
-        System.out.println("hii");
+        Scanner scan = new Scanner(System.in);
+        int n = nhapN(scan);
+        int a[] = nhapMang(n, scan);
+        xuatMang(a, n);
+        System.out.print("Nhap so can tim kiem x = "); 
+        int x = Integer.parseInt(scan.nextLine());
+        boolean found = linearSearch(a, x);
+        if(found) {
+            System.out.println("Tim thay x = " + x + " trong mang");
+        } else {
+            System.out.println("Khong tim thay x = " + x + " trong mang");
+        }
+    }
+
+    public static int nhapN(Scanner scan) {
+        int n;
+        do {
+            System.out.print("Vui long nhap n > 0: ");
+            n = Integer.parseInt(scan.nextLine());
+        } while(n < 0);
+        return n;
+    }
+
+    public static int[] nhapMang(int n, Scanner scan) {
+        int a[] = new int[n];
+        for(int i = 0; i < n; ++i) {
+            System.out.print("Vui long nhap a[" + i + "] = ");
+            a[i] = Integer.parseInt(scan.nextLine());
+        }
+        return a;
+    }
+
+    public static void xuatMang(int a[], int n) {
+        for(int i = 0; i < n; ++i) {
+            System.out.print(a[i] + "\t");
+        }
+        System.out.println("\n");
+    }
+
+    public static boolean linearSearch(int a[], int x) {
+        int n = a.length;
+        int i = 0;
+        while(i < n && a[i] != x) {
+            i++;
+        }
+        if(i == n) return false;
+        else return true;
     }
 }
