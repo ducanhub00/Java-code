f688b15fd6de4babe44676d58bfb2749ce2b6585
ducanhub00 <ducanhkhtn@gmail.com>
2021-03-29 08:51:35 +0700

    liet ke cac phan tu cach xa x

--- Online-TimKiem-BT4/src/App.java
+++ Online-TimKiem-BT4/src/App.java
@@ -1,21 +1,30 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        
-    }
+        Scanner scan = new Scanner(System.in);
+        int n = nhapN(scan);
+        int a[] = nhapMang(n);
+        xuatMang(a);
+        System.out.print("Vui long nhap so x can tinh: ");
+        int x = Integer.parseInt(scan.nextLine());
+        lietKe(a, x);
+    }   
 
 	public static int nhapN(Scanner scan) {
 		int n;
 		do {
-			System.out.println("Vui long nhap n: ");
+			System.out.print("Vui long nhap n > 0: ");
 			n = Integer.parseInt(scan.nextLine());
-		} while (n < 1 || n % 2 != 0);
+		} while (n < 1);
 		return n;
 	}
 
-	public static int[] nhapMang(int n) {
+	public static int[] nhapMang(int n, Scanner scan) {
 		int a[] = new int[n];
 		for (int i = 0; i < n; i++) {
-			a[i] = MIN + (int) ((Math.random()) * ((MAX - MIN) + 1));
+			System.out.print("a[" + i + "] = ");
+            a[i] = Integer.parseInt(scan.nextLine());
 		}
 		return a;
 	}
@@ -26,4 +35,25 @@ public class App {
 		}
 		System.out.println("\n");
 	}
+
+    public static void lietKe(int a[], int x) {
+        int index = -1; // chua tim thay
+        int b[] = new int[a.length];
+        for(int i = 0; i < a.length; ++i) {
+            b[i] = Math.abs(a[i] - x);
+        }
+        int max = 0;
+        for(int i = 0; i < a.length; ++i) {
+            if(b[i] > max) {
+                max = b[i];
+                index = i;
+            }
+        }
+        System.out.println("Cac phan tu cach xa x = " + x + "nam o cac vi tri: ");
+        for(int i = 0; i < a.length; ++i) {
+            if(b[i] == b[index]) {
+                System.out.print("(" + i + ", " + a[i] + ") \t");
+            }
+        }
+    }
 }
