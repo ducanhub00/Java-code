867d844a8829dc6f7eddae295c2a27f6595030d5
ducanhub00 <ducanhkhtn@gmail.com>
2021-03-31 09:23:35 +0700

    add file recursion

---
+++ Online-Recursion-BT1/src/App.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+
+    public static nhapN(Scanner scan) {
+        int n;
+        System.out.print("Vui long nhap so dia can chuyen: ");
+        do {
+            n = Integer.parseInt(scan.nextLine());
+        } while(n < 0);
+        return n;
+    }
+
+    
+}
