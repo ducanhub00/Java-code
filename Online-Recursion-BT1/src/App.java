8acff87c3016fc60f101bfda25f2b26f0783951d
ducanhub00 <ducanhkhtn@gmail.com>
2021-03-31 15:40:31 +0700

    thapHanoi

--- Online-Recursion-BT1/src/App.java
+++ Online-Recursion-BT1/src/App.java
@@ -2,7 +2,9 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner scan = new Scanner(System.in);
+        int n = nhapN(scan);
+        thapHanoi(n, 'A', 'B', 'C');
     }
 
     public static int nhapN(Scanner scan) {
@@ -14,5 +16,17 @@ public class App {
         return n;
     }
 
+    public static void doi1cot(int n, char a, char b) {
+        System.out.println("Chuyen dia thu " + n + "dia tu cot " + A + " sang cot " + B);
+    }
     
+    public static void thapHanoi(int n, char a, char b, char c) {
+        if(n == 1) {
+            doi1cot(n, a, c);
+        } else {
+            thapHanoi(n - 1, a, c, b);
+            doi1cot(n, a, c);
+            thapHanoi(n - 1, b, a, c);
+        }
+    }
 }
