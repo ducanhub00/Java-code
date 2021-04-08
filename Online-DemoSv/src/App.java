d8d6317f9dcffb9080a8fff04ddc530d822e9867
ducanhub00 <ducanhkhtn@gmail.com>
2021-04-08 20:26:03 +0700

    add constructor

--- Online-DemoSv/src/App.java
+++ Online-DemoSv/src/App.java
@@ -1,12 +1,24 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        SinhVien sV1 = new SinhVien();
-        SinhVien sV2 = new SinhVien();
+        Scanner scan = new Scanner(System.in);
+        // Su dung constructor khong tham so
+        SinhVien sV1 = new SinhVien(); // instance cua class SinhVien
+        SinhVien sV2 = new SinhVien(); // instance cua class SinhVien
         sV1.name = "Duc Anh";
         sV1.email = "ducanhkhtn@gamil.com";
         sV2.name = "Huyen Linh";
         sV2.email = "linhsociu031205@gmail.com";
         System.out.println("Ten: " + sV1.name + " - Email: " + sV1.email);
         System.out.println("Ten: " + sV2.name + " - Email: " + sV2.email);
+        
+        // Su dung constructor co tham so
+        System.out.print("Vui long nhap ten sinh vien: ");
+        String name = scan.nextLine();
+        System.out.print("Vui long nhap email sinh vien: ");
+        String email = scan.nextLine();
+        SinhVien sV3 = new SinhVien(name, email);
+        System.out.println("Ten: " + sV3.name + " - Email: " + sV3.email);
     }
 }
--- Online-DemoSv/src/SinhVien.java
+++ Online-DemoSv/src/SinhVien.java
@@ -1,4 +1,18 @@
 public class SinhVien {
     public String name;
     public String email;
+    //phuong thuc khoi tao mac dinh khong tham so
+    // constructor khong tham so
+    public SinhVien() {
+
+    }
+
+    //phuong thuc khoi tao su dung 2 tham so cho class SinhVien
+    // truyen du lieu cho thuoc tinh
+    // constructor co tham so
+
+    public SinhVien(String _name, String _email) {
+        this.name = _name; // this la dai dien cho instance duoc goi
+        this.email = _email;
+    }
 }
