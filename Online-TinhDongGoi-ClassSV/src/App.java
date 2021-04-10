a617ebfb1fd63e0ba3c49c6afe2ab71711c7ed80
ducanhub00 <ducanhkhtn@gmail.com>
2021-04-10 19:58:50 +0700

    fix demo tinhdonggoi

--- Online-TinhDongGoi-ClassSV/src/App.java
+++ Online-TinhDongGoi-ClassSV/src/App.java
@@ -1,7 +1,20 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        String st1 = new Student();
+        Scanner scan = new Scanner(System.in);
+        // information student 1
+        Student st1 = new Student();
         st1.setName("Duc Anh");
+        st1.setEmail("ducanhkhtn@gmail.com");
+
+        // information student 2
+        String ten = scan.nextLine();
+        String e = scan.nextLine();
+        Student st2 = new Student();
+        st2 = new Student(ten, e);
         
+        System.out.println("Ten: " + st1.getName() + " - Email: " + st1.getEmail());
+        System.out.println("Ten: " + st2.getName() + " - Email: " + st2.getEmail());
     }
 }
--- Online-TinhDongGoi-ClassSV/src/Student.java
+++ Online-TinhDongGoi-ClassSV/src/Student.java
@@ -1,13 +1,26 @@
+import java.util.jar.Attributes.Name;
+
 public class Student {
+    // thuoc tinh/bien thanh vien/ data members
     private String name;
     private String email;
-    private int mathScore;
-    private int chemistryScore;
-    private int physicScore;
+
+    // get, set methods
+    // accessor methods(phuong thuc truy xuat cac thuoc tinh/ bien thanh vien)
+    public String getName() {
+        return Name;
+    }
 
     public String getEmail() {
         return email;
     }
 
+    // phuong thuc thiet lap cac gia tri cac thuoc tinh/ bien thanh vien
+    public void setName(String _name) {
+        this.name = _name;
+    }
 
+    public void setEmail(String _email) {
+        this.email = _email;
+    }
 }
