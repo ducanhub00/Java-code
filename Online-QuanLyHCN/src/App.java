f603492231fb05191e4abf3c04b685223bfcaeac
ducanhub00 <ducanhkhtn@gmail.com>
2021-04-10 23:34:53 +0700

    su dung constructor co tham so

--- Online-QuanLyHCN/src/App.java
+++ Online-QuanLyHCN/src/App.java
@@ -3,8 +3,19 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner scan = new Scanner(System.in);
-        HinhChuNhat hcn = new HinhChuNhat();
+        // su dung constructor k tham so
+        /*HinhChuNhat hcn = new HinhChuNhat();
         hcn.nhap(scan);
+        hcn.tinhChuVi();
+        hcn.tinhDienTich();
+        hcn.xuat();*/
+        
+        // su dung constructor co tham so
+        System.out.print("Nhap chieu dai: ");
+        float cDai = Float.parseFloat(scan.nextLine());
+        System.out.print("Nhap chieu rong: ");
+        float cRong = Float.parseFloat(scan.nextLine());
+        HinhChuNhat hcn = new HinhChuNhat(cDai, cRong);
         hcn.tinhChuVi();
         hcn.tinhDienTich();
         hcn.xuat();
