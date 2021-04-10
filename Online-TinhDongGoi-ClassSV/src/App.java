663e4dff1aaf03a1b045ab73153ea0da66eee9cb
ducanhub00 <ducanhkhtn@gmail.com>
2021-04-10 22:35:20 +0700

    Finish tinh dong goi

--- Online-TinhDongGoi-ClassSV/src/App.java
+++ Online-TinhDongGoi-ClassSV/src/App.java
@@ -7,14 +7,21 @@ public class App {
         Student st1 = new Student();
         st1.setName("Duc Anh");
         st1.setEmail("ducanhkhtn@gmail.com");
-
-        // information student 2
-        // String ten = scan.nextLine();
-        // String e = scan.nextLine();
-        // Student st2 = new Student();
-        // st2 = new Student(ten, e);
         
-        System.out.println("Ten: " + st1.getName() + " - Email: " + st1.getEmail());
-        // System.out.println("Ten: " + st2.getName() + " - Email: " + st2.getEmail());
+        System.out.print("Vui long nhap diem toan: ");
+        float toan = Float.parseFloat(scan.nextLine());
+        st1.setMath(toan);
+
+        System.out.print("Vui long nhap diem ly: ");
+        float ly = Float.parseFloat(scan.nextLine());
+        st1.setPhysics(ly);
+
+        System.out.print("Vui long nhap diem hoa: ");
+        float hoa = Float.parseFloat(scan.nextLine());
+        st1.setChemistry(hoa);
+        
+        st1.tinhDiemTB();
+
+        System.out.println("Ten: " + st1.getName() + " - Email: " + st1.getEmail() + "\n" + "Diem trung binh: " + st1.getAverage());
     }
 }
--- Online-TinhDongGoi-ClassSV/src/Student.java
+++ Online-TinhDongGoi-ClassSV/src/Student.java
@@ -4,16 +4,40 @@ public class Student {
     // thuoc tinh/bien thanh vien/ data members
     private String name;
     private String email;
+    private float math;
+    private float physics;
+    private float chemistry;
+    private float averageScore;
 
+    //constructor
+    public Student() {
+
+    }
 
     // get, set methods
     // accessor methods(phuong thuc truy xuat cac thuoc tinh/ bien thanh vien)
     public String getName() {
-        return name;
+        return this.name;
     }
 
     public String getEmail() {
-        return email;
+        return this.email;
+    }
+
+    public float getMath() {
+        return this.math;
+    }
+
+    public float getPhysics() {
+        return this.physics;
+    }
+
+    public float getChemistry() {
+        return this.chemistry;
+    }
+
+    public float getAverage() {
+        return this.averageScore;
     }
 
     // phuong thuc thiet lap cac gia tri cac thuoc tinh/ bien thanh vien
@@ -23,5 +47,21 @@ public class Student {
 
     public void setEmail(String _email) {
         this.email = _email;
+    }
+
+    public void setMath(float mathScore) {
+        this.math = mathScore;
+    }
+
+    public void setPhysics(float physicScore) {
+        this.physics = physicScore;
+    }
+
+    public void setChemistry(float chemistryScore) {
+        this.chemistry = chemistryScore;
+    }
+
+    public void tinhDiemTB() {
+        this.averageScore = (this.chemistry + this.math + this.physics) / 3;
     }
 }
